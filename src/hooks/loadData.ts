d1c19c30cfccfaa8f881d3907ae6018e79f8aa76
UtopicRay <rayrm@gmail.com>
2024-12-20 11:53:45 -0500

    'Change cloud name'

--- src/hooks/loadData.ts
+++ src/hooks/loadData.ts
@@ -1,33 +1,34 @@
 'use server'
-import cloudinary from "cloudinary";
 import {ImageAPI} from "@/types";
+import cloudinaryAPI from "@/lib/cloudinaryAPI";
+
 
 export async function loadAlbums() {
-    return await cloudinary.v2.api.root_folders();
+    return await cloudinaryAPI.api.root_folders();
 }
 
 export async function loadImagesByAlbum(albumName: string) {
-    return await cloudinary.v2.api.resources_by_asset_folder(albumName, {fields: "tags"}) as { resources: ImageAPI[] };
+    return await cloudinaryAPI.api.resources_by_asset_folder(albumName, {fields: "tags"}) as { resources: ImageAPI[] };
 }
 
 export async function loadFavorites({max_results}: { max_results?: number }) {
     if (max_results) {
-        return await cloudinary.v2.search.expression("tags=favorite").fields('tags').sort_by('created_at', 'desc').max_results(max_results).execute() as {
+        return await cloudinaryAPI.search.expression("tags=favorite").fields('tags').sort_by('created_at', 'desc').max_results(max_results).execute() as {
             resources: ImageAPI[]
         };
     }
-    return await cloudinary.v2.search.expression("tags=favorite").fields('tags').sort_by('created_at', 'desc').execute() as {
+    return await cloudinaryAPI.search.expression("tags=favorite").fields('tags').sort_by('created_at', 'desc').execute() as {
         resources: ImageAPI[]
     };
 }
 
 export async function loadSearchImages({tag, max_result}: { tag?: string, max_result?: number }) {
     if (max_result) {
-        return await cloudinary.v2.search.expression(`${tag ? `tags=${tag}` : ""}`).fields('tags').sort_by('created_at', 'desc').max_results(max_result).execute() as {
+        return await cloudinaryAPI.search.expression(`${tag ? `tags=${tag}` : ""}`).fields('tags').sort_by('created_at', 'desc').max_results(max_result).execute() as {
             resources: ImageAPI[]
         }
     }
-    return await cloudinary.v2.search.expression(`${tag ? `tags=${tag}` : ""}`).fields('tags').sort_by('created_at', 'desc').execute() as {
+    return await cloudinaryAPI.search.expression(`${tag ? `tags=${tag}` : ""}`).fields('tags').sort_by('created_at', 'desc').execute() as {
         resources: ImageAPI[]
     }
 }
