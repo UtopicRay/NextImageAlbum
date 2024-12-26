0eb1b63660457094cf01e0d6022d3c8aba98544c
UtopicRay <rayrm@gmail.com>
2024-12-26 10:09:54 -0500

    'Change cloud name for PhotoApi'

--- src/hooks/loadData.ts
+++ src/hooks/loadData.ts
@@ -1,34 +1,34 @@
 'use server'
 import {ImageAPI} from "@/types";
-import cloudinaryAPI from "@/lib/cloudinaryAPI";
+import PhotoApi from "@/lib/PhotoApi";
 
 
 export async function loadAlbums() {
-    return await cloudinaryAPI.api.root_folders();
+    return await PhotoApi.api.root_folders();
 }
 
 export async function loadImagesByAlbum(albumName: string) {
-    return await cloudinaryAPI.api.resources_by_asset_folder(albumName, {fields: "tags"}) as { resources: ImageAPI[] };
+    return await PhotoApi.api.resources_by_asset_folder(albumName, {fields: "tags"}) as { resources: ImageAPI[] };
 }
 
 export async function loadFavorites({max_results}: { max_results?: number }) {
     if (max_results) {
-        return await cloudinaryAPI.search.expression("tags=favorite").fields('tags').sort_by('created_at', 'desc').max_results(max_results).execute() as {
+        return await PhotoApi.search.expression("tags=favorite").fields('tags').sort_by('created_at', 'desc').max_results(max_results).execute() as {
             resources: ImageAPI[]
         };
     }
-    return await cloudinaryAPI.search.expression("tags=favorite").fields('tags').sort_by('created_at', 'desc').execute() as {
+    return await PhotoApi.search.expression("tags=favorite").fields('tags').sort_by('created_at', 'desc').execute() as {
         resources: ImageAPI[]
     };
 }
 
 export async function loadSearchImages({tag, max_result}: { tag?: string, max_result?: number }) {
     if (max_result) {
-        return await cloudinaryAPI.search.expression(`${tag ? `tags=${tag}` : ""}`).fields('tags').sort_by('created_at', 'desc').max_results(max_result).execute() as {
+        return await PhotoApi.search.expression(`${tag ? `tags=${tag}` : ""}`).fields('tags').sort_by('created_at', 'desc').max_results(max_result).execute() as {
             resources: ImageAPI[]
         }
     }
-    return await cloudinaryAPI.search.expression(`${tag ? `tags=${tag}` : ""}`).fields('tags').sort_by('created_at', 'desc').execute() as {
+    return await PhotoApi.search.expression(`${tag ? `tags=${tag}` : ""}`).fields('tags').sort_by('created_at', 'desc').execute() as {
         resources: ImageAPI[]
     }
 }
--- src/hooks/use-actions.ts
+++ src/hooks/use-actions.ts
@@ -1,34 +1,34 @@
 'use server'
 import {revalidatePath} from "next/cache";
 import {ImageAPI} from "@/types";
-import cloudinaryAPI from "@/lib/cloudinaryAPI";
+import PhotoApi from "@/lib/PhotoApi";
 
 export async function setAsFavorite(public_id: string, isFavorite?: boolean) {
     if (isFavorite) {
-        await cloudinaryAPI.api.update(public_id, {tags: ''})
+        await PhotoApi.api.update(public_id, {tags: ''})
     } else {
-        await cloudinaryAPI.api.update(public_id, {tags: 'favorite'})
+        await PhotoApi.api.update(public_id, {tags: 'favorite'})
     }
     revalidatePath("/gallery");
 }
 
 export async function AddToAlbum({image, folder}:{image:ImageAPI,folder:string}) {
-    await cloudinaryAPI.api.create_folder(folder);
-    await cloudinaryAPI.api.update(image.public_id, { asset_folder:`${folder}`
+    await PhotoApi.api.create_folder(folder);
+    await PhotoApi.api.update(image.public_id, { asset_folder:`${folder}`
 }).then(result=>console.log(result));
 }
 export async function deleteImage(public_id:string): Promise<void> {
-    await cloudinaryAPI.api.delete_resources([public_id]);
+    await PhotoApi.api.delete_resources([public_id]);
 }
 
 export async function deleteAlbum(folder: string) {
-    const images = await cloudinaryAPI.api.resources_by_asset_folder(folder) as {
+    const images = await PhotoApi.api.resources_by_asset_folder(folder) as {
         resources: ImageAPI[]
     };
     if (images) {
         images.resources.forEach(async (image: ImageAPI) => {
-            await cloudinaryAPI.api.update(image.public_id, { asset_folder:`/`}).then(results => {console.log(results);});
+            await PhotoApi.api.update(image.public_id, { asset_folder:`/`}).then(results => {console.log(results);});
         })
     }
-    await cloudinaryAPI.api.delete_folder(folder);
+    await PhotoApi.api.delete_folder(folder);
 }
